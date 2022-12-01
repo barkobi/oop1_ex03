1c99063947065e7b42700dca9d2cb8a84e33cc66
ofek4454 <ofek4454@gmail.com>
2022-12-01 12:57:42 +0200

    vector operators start

--- src/Vector.h
+++ src/Vector.h
@@ -7,24 +7,21 @@ public:
     // constructors / destructors:
     explicit Vector(int size = 0, const Zi& init = {});
     Vector(Zi arr[], int size);
-    Vector(const Vector &old);
-    ~Vector();
 
     //getters / setters:
     int getSize() const;
-    bool isEmpty() const;
 
     // operators:
-    Array operator[](int index) const;
-    Array& operator[](int index);
+    Zi operator[](int index) const;
+    Zi& operator[](int index);
 
 private:
-    Array *m_arr;
-    int m_size;
+    Array m_arr;
 };
 
 Vector operator+(const Vector &v1, const Vector &v2);
 Vector operator-(const Vector &v1, const Vector &v2);
+Vector operator-(const Vector &v);
 Vector operator*(const Vector &v1, const Vector &v2);
 Vector operator*(const Vector &v, int s);
 Vector operator*(int s, const Vector &v);
@@ -32,7 +29,6 @@ Vector& operator+=(Vector v1, const Vector &v2);
 Vector& operator-=(Vector v1, const Vector &v2);
 Vector& operator*=(Vector v1, const Vector &v2);
 Vector& operator*=(Vector v, int s);
-Vector& operator-(const Vector &v2);
 bool operator==(const Vector &v1, const Vector &v2);
 bool operator!=(const Vector &v1, const Vector &v2);
 
